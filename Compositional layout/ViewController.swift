4097211530ffd135a14d9932421d674f666ad1f5
Ahmadxon <ahmadxonqodirov0113@gmail.com>
2023-04-17 17:47:31 +0500

    Initial Commit

---
+++ Compositional layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Compositional layout
+//
+//  Created by Ahmadxon Qodirov on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
